886468d5d64ad02241e7c91b7eb7f85c0c563762
Marco Cano <artofmarco@gmail.com>
2022-02-18 12:00:13 -0800

    fix: :hammer: overflow suggestions

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-center items-center flex-wrap container m-auto p-4 md:p-20 darkMode min-h-screen">
+    <div class="flex justify-center items-center flex-wrap p-4 md:p-20 darkMode min-h-screen">
         <template v-for="resource in filters['@type']" :key="resource.term">
             <ResourceCard :title="resource.term" :sourceInfo="resource"></ResourceCard>
         </template>
